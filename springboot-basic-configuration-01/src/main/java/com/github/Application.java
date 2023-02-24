12fb35f2d6e5188470f49f039410681e06c89b68
许大仙 <1900919313@qq.com>
2023-02-24 15:29:20 +0800

    📝:  SpringBoot 基础配置

--- springboot-basic-configuration-01/src/main/java/com/github/Application.java
+++ springboot-basic-configuration-01/src/main/java/com/github/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 测试 GitLab 能否同步到 GitHub 中
+ *
  * @author 许大仙
  * @version 1.0
  * @since 2023-02-24 13:15
