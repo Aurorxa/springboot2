cee886cc86fe8e83a4fae2b5a3221473a3c60d07
许大仙 <1900919313@qq.com>
2023-02-24 14:32:52 +0800

    📝: SpringBoot 入门案例

--- springboot-base-02/src/main/java/com/github/Application.java
+++ springboot-base-02/src/main/java/com/github/Application.java
@@ -1,7 +1,9 @@
 package com.github;
 
+import com.github.web.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author 许大仙
@@ -11,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        // ConfigurableApplicationContext 其实就是 Spring 的容器
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        // 从 Spring 容器中获取 Bean
+        BookController bean = context.getBean(BookController.class);
+        System.out.println("bean = " + bean);
     }
 }
