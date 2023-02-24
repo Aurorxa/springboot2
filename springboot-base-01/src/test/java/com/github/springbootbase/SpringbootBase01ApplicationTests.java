53288b1e2419bde03f358bf7eb5d81dfd0ce5e74
许大仙 <1900919313@qq.com>
2023-02-24 14:31:18 +0800

    📝: SpringBoot 入门案例

---
+++ springboot-base-01/src/test/java/com/github/springbootbase/SpringbootBase01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springbootbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBase01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
