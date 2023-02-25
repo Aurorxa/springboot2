498e6f06c1a29aa0595d7e9cfae2c6e27ccc62c4
许大仙 <1900919313@qq.com>
2023-02-25 23:52:39 +0800

    SpringBoot 整合 Junit5

--- springboot-junit5/src/test/java/com/github/ApplicationTests.java
+++ springboot-junit5/src/test/java/com/github/ApplicationTests.java
@@ -1,14 +1,16 @@
 package com.github;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 public class ApplicationTests {
 
     @Test
     public void contextLoads() {
-
+        log.info("{}", "你好啊");
     }
 
 }
