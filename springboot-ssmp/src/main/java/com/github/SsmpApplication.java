1f335cb355b14008cbfde5209a173285933f2b97
许大仙 <1900919313@qq.com>
2023-02-27 08:26:52 +0800

    🎨: 基于 SpringBoot 的 SSMP 整合案例

---
+++ springboot-ssmp/src/main/java/com/github/SsmpApplication.java
@@ -0,0 +1,18 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 许大仙
+ * @version 1.0
+ * @since 2023-02-26 07:42
+ */
+@SpringBootApplication
+public class SsmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmpApplication.class, args);
+    }
+
+}
---
+++ springboot-ssmp/src/main/java/com/github/domain/Book.java
@@ -0,0 +1,38 @@
+package com.github.domain;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.gitee.zerowsh.actable.annotation.AcColumn;
+import io.gitee.zerowsh.actable.annotation.AcTable;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author 许大仙
+ * @version 1.0
+ * @since 2023-02-27 08:20
+ */
+@Data
+@TableName("`Book`")
+@AcTable(name = "`Book`", comment = "图书")
+public class Book implements Serializable {
+
+    @AcColumn(name = "`id`", comment = "主键")
+    @TableField("`id`")
+    private Integer id;
+
+    @AcColumn(name = "`type`", comment = "图书类型")
+    @TableField("`type`")
+    private String type;
+
+    @AcColumn(name = "`name`", comment = "图书名称")
+    @TableField("`name`")
+    private String name;
+
+    @AcColumn(name = "`description`", comment = "图书描述")
+    @TableField("`description`")
+    private String description;
+
+
+}
