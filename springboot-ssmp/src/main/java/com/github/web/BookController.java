de5dfe62e8254b047019a646b4b09e3675f50ade
许大仙 <1900919313@qq.com>
2023-02-27 14:39:17 +0800

    🎨: 基于 SpringBoot 的 SSMP 整合案例

--- springboot-ssmp/src/main/java/com/github/web/BookController.java
+++ springboot-ssmp/src/main/java/com/github/web/BookController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @author 许大仙
  * @version 1.0
@@ -27,11 +29,17 @@ public class BookController {
     @NonNull
     private BookService bookService;
 
-
     @ApiOperation("查询详情接口")
     @GetMapping(value = "/view/{id}")
     public Result<Book> view(@PathVariable("id") @ApiParam(value = "主键") Integer id) {
         return Result.success(this.bookService.getById(id));
     }
+
+    @ApiOperation("查询全部数据")
+    @GetMapping(value = "/getAll")
+    public Result<List<Book>> getAll() {
+        return Result.success(this.bookService.list());
+    }
+
 
 }
