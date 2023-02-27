49b3da1d22627e0dcfea2c8fcb3851a27f11a517
许大仙 <1900919313@qq.com>
2023-02-27 09:51:02 +0800

    🎨: 基于 SpringBoot 的 SSMP 整合案例

---
+++ springboot-ssmp/src/main/java/com/github/config/MybatisPlusConfiguration.java
@@ -0,0 +1,26 @@
+package com.github.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 许大仙
+ * @version 1.0
+ * @since 2023-02-27 09:46
+ */
+@Configuration
+public class MybatisPlusConfiguration {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+
+}
