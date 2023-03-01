44c9a7003a2509fd22ab0aedada46fa9a8c5b258
许大仙 <1900919313@qq.com>
2023-03-02 00:08:45 +0800

    多环境开发

---
+++ springboot-config-gradle/src/main/java/com/github/SpringbootConfigGradleApplication.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfigGradleApplication.class, args);
+    }
+
+}
