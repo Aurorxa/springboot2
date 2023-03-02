bbbfe503e03207c9e3f909f0900bc692cfece806
许大仙 <1900919313@qq.com>
2023-03-02 13:17:19 +0800

    🎨: 配置文件高级

---
+++ springboot-config2/src/main/java/com/github/SpringbootConfig2Application.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfig2Application.class, args);
+    }
+
+}
