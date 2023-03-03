2f3c4c9f7ac8e0975cfd12722b06c8e8124dbcb6
许大仙 <1900919313@qq.com>
2023-03-03 10:38:20 +0800

    : 配置文件高级

--- springboot-config2/src/main/java/com/github/ex/GlobalExceptionAdvice.java
+++ springboot-config2/src/main/java/com/github/ex/GlobalExceptionAdvice.java
@@ -2,8 +2,17 @@ package com.github.ex;
 
 import com.github.rest.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常捕获
@@ -40,4 +49,50 @@ public class GlobalExceptionAdvice {
         ex.printStackTrace();
         return Result.error(ex.getMessage());
     }
+
+    /**
+     * 处理 form data方式调用接口校验失败抛出的异常
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(BindException.class)
+    public Result<?> handleBindException(BindException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        String msg = bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(";"));
+        log.info("GlobalExceptionAdvice ===> handleBindException 的异常信息是 {}", msg);
+        return Result.error(msg);
+    }
+
+    /**
+     * 处理 json 请求体调用接口校验失败抛出的异常
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        String msg = bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(";"));
+        log.info("GlobalExceptionAdvice ===> handleMethodArgumentNotValidException 的异常信息是 {}", msg);
+        return Result.error(msg);
+    }
+
+    /**
+     * 处理单个参数校验失败抛出的异常
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Result<?> constraintViolationExceptionHandler(ConstraintViolationException ex) {
+        Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+        String msg =
+                constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(";"));
+        log.info("GlobalExceptionAdvice ===> constraintViolationExceptionHandler 的异常信息是 {}", msg);
+        return Result.error(msg);
+    }
+
 }
