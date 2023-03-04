8729e9df9b896b40ae18ad6161711c8d22dd7ddd
许大仙 <1900919313@qq.com>
2023-03-04 14:23:31 +0800

    : SpringBoot的内置数据源

--- springboot-redis/src/test/java/com/github/SpringbootRedisApplicationTests.java
+++ springboot-redis/src/test/java/com/github/SpringbootRedisApplicationTests.java
@@ -1,14 +1,27 @@
 package com.github;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Map;
 
 @SpringBootTest
-class SpringbootRedisApplicationTests {
+public class SpringbootRedisApplicationTests {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     public void test() {
+        Map<String, Object> map = Map.of("hello", "world");
 
+        String key = "redis";
+        this.redisTemplate.opsForHash().putAll(key, map);
+
+        Map<Object, Object> entries = this.redisTemplate.opsForHash().entries(key);
+        System.out.println("entries = " + entries);
     }
 
 }
