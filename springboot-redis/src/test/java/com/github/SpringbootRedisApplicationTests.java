4de93469999be042db413d946c68855488668a95
许大仙 <1900919313@qq.com>
2023-03-04 14:28:02 +0800

    : SpringBoot的内置数据源

--- springboot-redis/src/test/java/com/github/SpringbootRedisApplicationTests.java
+++ springboot-redis/src/test/java/com/github/SpringbootRedisApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.Map;
 
@@ -13,14 +14,28 @@ public class SpringbootRedisApplicationTests {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void test() {
-        Map<String, Object> map = Map.of("hello", "world");
+        Map<Object, Object> map = Map.of("hello", "world");
 
         String key = "redis";
         this.redisTemplate.opsForHash().putAll(key, map);
 
         Map<Object, Object> entries = this.redisTemplate.opsForHash().entries(key);
+        System.out.println("entries = " + entries);
+    }
+
+
+    @Test
+    public void test2() {
+        Map<String, String> map = Map.of("hello", "world");
+        String key = "redis";
+        this.stringRedisTemplate.opsForHash().putAll(key, map);
+
+        Map<Object, Object> entries = this.stringRedisTemplate.opsForHash().entries(key);
         System.out.println("entries = " + entries);
     }
 
