1fe557bedafdbace9cc34001a99dd132dad0f5c6
许大仙 <1900919313@qq.com>
2023-03-06 11:19:27 +0800

    : SpringBoot的内置数据源

---
+++ springboot-es7/src/test/java/com/github/springbootes7/SpringbootEs7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springbootes7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEs7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
